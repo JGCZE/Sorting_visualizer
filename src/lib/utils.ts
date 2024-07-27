5e491dcd1ee906bcc1cdc2bcaeff976856ad95e8
Jan Gross <jan.gross.cze@gmail.com>
2024-07-27 11:23:16 +0200

    insertion sort

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { generateBubbleSortAnimationArray } from "@/algorithms/bubbleSort";
 import { AnimationArrayType, SortingAlgorithmType } from "./types";
 import { generateSelectionSortAnimationArray } from "@/algorithms/selectionSort";
+import { generateInsertionSortAnimationArray } from "@/algorithms/insertionSort";
 
 export const MIN_ANIMATION_SPEED = 100;
 export const MAX_ANIMATION_SPEED = 400;
@@ -29,6 +30,9 @@ export function generateAnimationArray(
       break;
     case "selection":
       generateSelectionSortAnimationArray(isSorting, array, runAnimation);
+      break;
+    case "insertion":
+      generateInsertionSortAnimationArray(isSorting, array, runAnimation);
       break;
     default:
       break;
